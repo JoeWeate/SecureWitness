452dc2323da5686d6b2d8149b1f08a47995721f6
zeizyy <yz9fy@virginia.edu>
2015-03-29 23:39:20 -0400

    fix little bug

--- mysite/SecureWitness/views.py
+++ mysite/SecureWitness/views.py
@@ -9,5 +9,5 @@ def index(request):
     report_list = Report.objects.order_by('-pub_date')[:5]
     output = ""
     for p in report_list:
-        output="Author: "+p.author+'+"Published date: "+str(p.pub_date)+'\n'+"Content: "+p.content+'\n'
+        output="Author: "+p.author+'\n'+"Published date: "+str(p.pub_date)+'\n'+"Content: "+p.content+'\n'
     return HttpResponse(output)

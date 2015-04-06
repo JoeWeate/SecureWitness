71066718118f68e026d308dabc745d32f0d21964
NG <username@users.noreply.github.com>
2015-04-06 02:04:50 -0400

    Added readable permission to report, privacy attribute to report

--- mysite/SecureWitness/models.py
+++ mysite/SecureWitness/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Report(models.Model):
-    author = models.CharField(max_length=30)
-    pub_date = models.DateTimeField('date published')
-    content = models.CharField(max_length=200)
+	author = models.CharField(max_length=30)
+	pub_date = models.DateTimeField('date published')
+	content = models.CharField(max_length=200)
+	privacy = models.BooleanField(default=True)
 
+	class Meta:
+		permissions = (
+			("can_read", "Permission to read file"),
+		)
 class Document(models.Model):
 	docfile = models.FileField(upload_to='documents/%Y/%m/%d')

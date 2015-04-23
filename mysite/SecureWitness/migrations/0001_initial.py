2c40e046bff0b28a7618636f4bf25b43a56f1fca
zeizyy <yz9fy@virginia.edu>
2015-04-22 20:40:44 -0400

    polish

--- mysite/SecureWitness/migrations/0001_initial.py
+++ mysite/SecureWitness/migrations/0001_initial.py
@@ -2,22 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import datetime
 from django.conf import settings
+import datetime
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('content', models.CharField(max_length=200)),
                 ('pub_date', models.DateTimeField(default=datetime.datetime.today)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Document',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=200)),
                 ('docfile', models.FileField(upload_to='documents/%Y/%m/%d')),
                 ('encrypted', models.BooleanField(default=False)),
@@ -43,7 +43,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Folder',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('name', models.CharField(max_length=200)),
                 ('pub_date', models.DateTimeField(default=datetime.datetime.today)),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
@@ -55,7 +55,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Keyword',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('word', models.CharField(max_length=200)),
             ],
             options={
@@ -65,17 +65,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Report',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
                 ('pub_date', models.DateTimeField(default=datetime.datetime.today)),
-                ('inc_date', models.DateTimeField(null=True, blank=True)),
+                ('inc_date', models.DateTimeField(blank=True, null=True)),
                 ('short', models.CharField(max_length=200)),
                 ('detailed', models.CharField(max_length=2000)),
                 ('privacy', models.BooleanField(default=True)),
-                ('location', models.CharField(max_length=200, blank=True)),
+                ('location', models.CharField(blank=True, max_length=200)),
                 ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-                ('doc', models.ManyToManyField(to='SecureWitness.Document', null=True, blank=True)),
-                ('groups', models.ManyToManyField(to='auth.Group', null=True, blank=True)),
-                ('keyword', models.ManyToManyField(to='SecureWitness.Keyword', null=True, blank=True)),
+                ('doc', models.ManyToManyField(blank=True, null=True, to='SecureWitness.Document')),
+                ('groups', models.ManyToManyField(blank=True, null=True, to='auth.Group')),
+                ('keyword', models.ManyToManyField(blank=True, null=True, to='SecureWitness.Keyword')),
             ],
             options={
                 'permissions': (('can_read', 'Permission to read file'), ('can_search', 'Permission to search for file')),
@@ -85,8 +85,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(primary_key=True, auto_created=True, serialize=False, verbose_name='ID')),
-                ('activation_key', models.CharField(max_length=40, blank=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', primary_key=True, auto_created=True)),
+                ('activation_key', models.CharField(blank=True, max_length=40)),
                 ('key_expires', models.DateTimeField(default=datetime.date(2015, 4, 22))),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
@@ -98,7 +98,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='folder',
             name='reports',
-            field=models.ManyToManyField(to='SecureWitness.Report', null=True, blank=True),
+            field=models.ManyToManyField(blank=True, null=True, to='SecureWitness.Report'),
             preserve_default=True,
         ),
         migrations.AddField(
